e8d3305af524421e98c09209a584e0f1bc324aac
scarylp666 <78630467+scarylp666@users.noreply.github.com>
2022-01-04 18:32:59 +0100

    Update spracovanie.py

--- spracovanie.py
+++ spracovanie.py
@@ -3,19 +3,20 @@ import matplotlib.pyplot as plt
 from datetime import date
 from datetime import datetime
 import re
-url = "http://192.168.1.232/"
-url1 = "http://192.168.1.232/output.txt"
-today = date.today()
-now = datetime.now()
-hour = int(today.strftime("%I"))
-current_time = now.strftime("%H:%M")
-r = requests.get(url)
-r1 = requests.get(url1)
-r2 = re.findall(r"(\d+\.\d+)",r1.text)
-x1 =  current_time
-y1 = float(r2[0])
+tempreature = open("saved_dataT.txt","r")
+humidity = open("saved_dataH.txt","r")
+time = open("saved_time.txt","r")
+#today = date.today()
+#now = datetime.now()
+#hour = int(today.strftime("%I"))
+#current_time = now.strftime("%H:%M")
+#r = requests.get(url)
+#r1 = requests.get(url1)
+#2 = re.findall(r"(\d+\.\d+)",r1.text)
+x1 = str(time)
+y1 = str(tempreature)
 plt.scatter(x1, y1, label= "Teplota", color= "red",marker= "+", s=30)
-y2 = float(r2[1])
+y2 = str(humidity)
 plt.scatter(x1, y2, label= "Vlhkost", color= "green",marker= "+", s=30)
 plt.xlabel('x - Hodiny')
 plt.ylabel('y - Hodnota')
