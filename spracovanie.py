3d41e4d0c7dc1ad83af4373d30d136fd3fc9433e
scarylp666 <78630467+scarylp666@users.noreply.github.com>
2022-01-04 23:06:41 +0100

    Update spracovanie.py

--- spracovanie.py
+++ spracovanie.py
@@ -2,28 +2,19 @@ import requests
 import matplotlib.pyplot as plt
 from datetime import date
 from datetime import datetime
+import numpy as np
 import re
 tempreature = open("saved_dataT.txt","r")
 humidity = open("saved_dataH.txt","r")
 time = open("saved_time.txt","r")
-#today = date.today()
-#now = datetime.now()
-#hour = int(today.strftime("%I"))
-#current_time = now.strftime("%H:%M")
-#r = requests.get(url)
-#r1 = requests.get(url1)
-#2 = re.findall(r"(\d+\.\d+)",r1.text)
-x1 = str(time)
-y1 = str(tempreature)
-plt.scatter(x1, y1, label= "Teplota", color= "red",marker= "+", s=30)
-y2 = str(humidity)
-plt.scatter(x1, y2, label= "Vlhkost", color= "green",marker= "+", s=30)
+tempreature1 = list(np.float_(tempreature.read().split(",")[:-1]))
+humidity1 = list(np.float_(humidity.read().split(",")[:-1]))
+time1 = (time.read().split(",")[:-1])
+plt.plot(time1, tempreature1, label = "teplota",marker='o', markerfacecolor='red', markersize=5)
+plt.plot(time1, humidity1, label = "vlhkost",marker='o', markerfacecolor='green', markersize=5)
 plt.xlabel('x - Hodiny')
 plt.ylabel('y - Hodnota')
 plt.title('Zmena vlhkosti a teploty v priebehu dna')
 plt.legend()
-plt.grid(color = 'green', linestyle = '--', linewidth = 0.5)
+plt.grid(color = 'purple', linestyle = '--', linewidth = 0.5)
 plt.show()
-
-#print(r2)
-#print(r1)
