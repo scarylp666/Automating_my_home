5bdd1bb38f1cf6f052a3aa75e4866218325052b8
scarylp666 <78630467+scarylp666@users.noreply.github.com>
2022-01-04 23:06:58 +0100

    Update saving.py

--- saving.py
+++ saving.py
@@ -4,24 +4,20 @@ import time
 import re
 import requests
 url1 = "http://192.168.1.232/output.txt"
-#r1 = requests.get(url1)
-#r2 = re.findall(r"(\d+\.\d+)",r1.text)
-file1 = open("saved_dataT.txt","w")
-file2 = open("saved_dataH.txt","w")
-file3 = open("saved_time.txt","w")
-
-for i in range (8):
-	r1 = requests.get(url1)
-	r2 = re.findall(r"(\d+\.\d+)",r1.text)
-	today = date.today()
-	now = datetime.now()
-	hour = int(today.strftime("%I"))
-	current_day = now.strftime("%d.%m.%y/%H:%M")
-	current_time = now.strftime("%H:%M")
-	file1.write(str(r2[0]))
-	file2.write(str(r2[1]))
-	file3.write(current_day)
-	file1.write(",")
-	file2.write(",")
-	file3.write(",")
-	time.sleep(10)
+with open("saved_dataT.txt","w") as file1:
+	with open("saved_dataH.txt","w") as file2:
+		with open("saved_time.txt","w") as file3:
+			for i in range (30):
+				r1 = requests.get(url1)
+				r2 = re.findall(r"(\d+\.\d+)",r1.text)
+				today = date.today()
+				now = datetime.now()
+				hour = int(today.strftime("%I"))
+				current_day = now.strftime("%H:%M:%S")
+				file1.write(str(r2[0]))
+				file2.write(str(r2[1]))
+				file3.write(current_day)
+				file1.write(",")
+				file2.write(",")
+				file3.write(",")
+				time.sleep(15)
