5f0aad42a4ed318b3aecaa9000962aacc0bf51a6
scarylp666 <78630467+scarylp666@users.noreply.github.com>
2022-01-08 01:56:30 +0100

    Update saving.py

--- saving.py
+++ saving.py
@@ -4,20 +4,25 @@ import time
 import re
 import requests
 url1 = "http://192.168.1.232/output.txt"
+i = 0
 with open("saved_dataT.txt","w") as file1:
 	with open("saved_dataH.txt","w") as file2:
 		with open("saved_time.txt","w") as file3:
-			for i in range (24):
+			while i < 24:
 				r1 = requests.get(url1)
 				r2 = re.findall(r"(\d+\.\d+)",r1.text)
 				today = date.today()
 				now = datetime.now()
 				hour = int(today.strftime("%I"))
-				current_day = now.strftime("%H:%M:%S")
-				file1.write(str(r2[0]))
-				file2.write(str(r2[1]))
-				file3.write(current_day)
-				file1.write(",")
-				file2.write(",")
-				file3.write(",")
+				current_day = now.strftime("%d.%m.%y-%H:%M")
+				if len(r2) > 0:
+					file1.write(str(r2[0]))
+					file2.write(str(r2[1]))
+					file3.write(current_day)
+					file1.write(",")
+					file2.write(",")
+					file3.write(",")
+					i+=1
+				else:
+					continue
 				time.sleep(3600)
